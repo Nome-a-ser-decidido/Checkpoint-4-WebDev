3a487972f2e4f0632ce764ba95a4277700263885
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-03-19 08:33:41 -0300

    Criando a estrutura e css do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import "../css/header.css"
+import "../assets/Vasco.png"
 
 const Nav = () => {
   return (
     <header>
-      <img src="../assets/logo.png" alt="" />
+      <div>Home</div>
+       <img src="src/assets/logo.png" alt="" />
+       <div class="produto-sobre">
+        <div>Produtos</div>
+        <div>Sobre</div>
+       </div>
     </header>
   )
 }
