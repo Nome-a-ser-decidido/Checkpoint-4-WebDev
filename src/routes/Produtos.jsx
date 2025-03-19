f79d7ce35898d3da06fc620ec7ef1fac983bba3c
Guilherme Vital Rodrigues <guivitalrodrigues@gmail.com>
2025-03-19 14:53:04 -0300

    Criando a pagina produtos.jsx

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import '../css/produtos.css'
 
 const Produtos = () => {
   return (
-    <div>
-      
+    <div className="container">
+    <div className="destaque">
+      <img src="../src/assets/logo.png" alt="Camisa Vasco" className="img-destaque" />
+      <div className="info">
+        <p>Camisa do Vasco, confortável e moderna.</p>
+        <p className="preco">R$ 129.90</p>
+        <button className="comprar">COMPRE JÁ</button>
+      </div>
     </div>
+
+    <div className="produtos-grid">
+      <div className="produto">
+        <img src="../src/assets/logo.png" alt="Camisa Vasco" className="img-produto" />
+        <p>Camisa Vasco Oficial</p>
+        <p className="preco">R$ 129.90</p>
+      </div>
+      <div className="produto">
+        <img src="../src/assets/logo.png" alt="Camisa Vasco Preta" className="img-produto" />
+        <p>Camisa Vasco Preta</p>
+        <p className="preco">R$ 179.00</p>
+      </div>
+      <div className="produto">
+        <img src="../src/assets/logo.png" alt="Camisa Vasco Dourada" className="img-produto" />
+        <p>Camisa Vasco Dourada</p>
+        <p className="preco">R$ 129.90</p>
+      </div>
+    </div>
+  </div>
+    
   )
 }
 
