939ecf1a48e78cba7e0db974ead5e46e7f6da172
Lucas <lguerreiroms@gmail.com>
2025-03-20 10:29:15 -0300

    Criando o sobre

--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -5,16 +5,17 @@ const Sobre = () => {
   return (
     <section>
       <div class="texto">
-        <h1>Sobre</h1>
-      
-        <a>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-        "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-        </a>
+        <h1 class="titulovascao">Sobre</h1>
+        <div class="text">
+          <img class="vascao" src="src/assets/Vasco.png" alt="VascaoDoPeito" />
+          <p class="textovascao">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."<br></br>
+          "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
+          </p>
       </div>
-      <div>
-
-        <img src="src/assets/Vasco.png" alt="VascaoDoPeito" />
       
+
+          
+     
       </div>
     </section>
   )
